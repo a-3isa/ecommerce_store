5702886ed45f81bb0990af0a13ee24d0aa01d1ce
a-3isa <a7med3isa000@gmail.com>
2025-10-07 03:09:57 +0300

    modified:   .gitignore 	modified:   TODO.md 	modified:   package-lock.json 	modified:   package.json 	modified:   src/auth/jwt-auth.guard.ts 	modified:   src/cart/cart.module.ts 	modified:   src/cart/cart.service.ts 	modified:   src/cart/dto/update-cart.dto.ts 	modified:   src/cart/entities/cart-item.entity.ts 	modified:   src/cart/entities/cart.entity.ts 	modified:   src/main.ts 	modified:   src/order/dto/create-order.dto.ts 	modified:   src/order/dto/update-order.dto.ts 	modified:   src/order/entities/order.entity.ts 	modified:   src/order/order.controller.ts 	modified:   src/order/order.module.ts 	modified:   src/order/order.service.ts 	modified:   src/product-attr-val/entities/product-attr-val.entity.ts 	modified:   src/product-attr-var/dto/create-product-attr-var.dto.ts 	modified:   src/product-attr-var/entities/product-attr-var.entity.ts 	modified:   src/product-attr-var/product-attr-var.service.ts 	modified:   src/product/entities/product.entity.ts 	modified:   src/product/product.service.ts 	modified:   src/user/dto/update-user.dto.ts 	modified:   src/user/entities/user.entity.ts 	modified:   src/user/user.service.ts 	src/order/entities/order-item.entity.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ProductAttrValModule } from './product-attr-val/product-attr-val.module
 import { ProductAttrVarModule } from './product-attr-var/product-attr-var.module';
 import { GiftModule } from './gift/gift.module';
 import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -41,6 +42,7 @@ import { CartModule } from './cart/cart.module';
     ProductAttrVarModule,
     GiftModule,
     CartModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [
